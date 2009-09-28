4ba961e1998c7d21bfb7f54d89fcb6321ba870bd
fox000002 <fox000002@6236664e-9beb-11de-8451-2789e535d5ff>
2009-09-28 13:07:09 +0000

    Add CheckBoxForm.

---
+++ trunk/hycs/form/checkbox.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+
+class CheckBoxForm : Form
+{
+    private CheckBox ckbox;
+
+    public CheckBoxForm()
+    {
+        this.Text = "checkbox";
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.ControlBox = true;
+
+        ckbox = new CheckBox();
+        ckbox.Text = "off";
+        ckbox.Left = this.ClientRectangle.Width - ckbox.Width  - 100;
+        ckbox.Top = this.ClientRectangle.Height - ckbox.Height - 100;
+        ckbox.Appearance = Appearance.Button;
+        ckbox.BackColor = Color.Red;
+        ckbox.Click += new EventHandler(ckbox_Click);
+        this.Controls.Add(ckbox);
+    }
+
+    private void ckbox_Click(object sender, EventArgs e)
+    {
+        this.ckbox.Text = "On";
+    }
+
+    public static void Main()
+    {
+        Application.Run(new CheckBoxForm());
+    }
+}
+
