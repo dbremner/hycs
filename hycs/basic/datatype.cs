cf85f933e625bfa4a1d214a73027f9c02bb13b8d
fox000002 <fox000002@6236664e-9beb-11de-8451-2789e535d5ff>
2010-01-04 16:48:53 +0000

    Add datatype.cs and flash.cs.

---
+++ hycs/basic/datatype.cs
@@ -0,0 +1,45 @@
+class DataType
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Data Types in CSharp:");
+
+        // value types
+        /* 
+            value-type:
+                struct-type
+                enum-type
+            struct-type:
+                type-name
+                simple-type
+            simple-type:
+                numeric-type
+                bool
+            numeric-type:
+                integral-type
+                floating-point-type
+                decimal
+            integral-type:
+                sbyte
+                byte
+                short
+                ushort
+                int
+                uint
+                long
+                ulong
+                char
+            floating-point-type:
+                float
+                double
+            enum-type:
+                type-name 
+         */
+        int i = 10;
+        int j = new int();
+
+        System.Console.WriteLine("i = {0:D}, j = {1:D}", i, j);
+
+        System.Console.ReadKey();
+    }
+}
